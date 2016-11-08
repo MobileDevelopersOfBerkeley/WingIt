b35a33137dd2f41b931a3d60e7f1edc57db1072e
Sharie Wang <Sharie Wang>
2016-11-07 23:14:10 -0800

    login layout files

---
+++ app/src/main/java/com/mdb/wingit/wingit/Login.java
@@ -0,0 +1,24 @@
+package com.mdb.wingit.wingit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //UI elements
+        EditText email = (EditText) findViewById(R.id.email);
+        EditText password = (EditText) findViewById(R.id.password);
+        Button login = (Button) findViewById(R.id.sign_in_button);
+        TextView forgotPass = (TextView) findViewById(R.id.textView);
+
+        //Todo: Complete bottom gray bar where you can sign up
+    }
+}
