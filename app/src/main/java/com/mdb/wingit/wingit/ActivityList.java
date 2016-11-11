a24ef15cbae6275d798eced9c5e47b1042224522
Shreya Reddy <reddy.shreya18@gmail.com>
2016-11-11 14:26:19 -0800

    Added Activity class

---
+++ app/src/main/java/com/mdb/wingit/wingit/ActivityList.java
@@ -0,0 +1,43 @@
+package com.mdb.wingit.wingit;
+
+import java.util.ArrayList;
+
+/**
+ * Created by reddy on 11/11/2016.
+ */
+
+public class ActivityList {
+    public ArrayList<Activity> activityArrayList;
+
+    public ActivityList() {
+        this.activityArrayList = new ArrayList<>();
+    }
+
+    public ArrayList<Activity> getArrayList() {
+        return this.activityArrayList;
+    }
+
+    public static class Activity {
+        String name;
+        String time;
+        String pictureURL;
+
+        public Activity(String name, String time, String pictureURL) {
+            this.name = name;
+            this.time = time;
+            this.pictureURL = pictureURL;
+        }
+
+        public static String getName() {
+            return this.name;
+        }
+
+        public static String getTime() {
+            return this.time;
+        }
+
+        public static String getPictureURL() {
+            return this.pictureURL;
+        }
+    }
+}
