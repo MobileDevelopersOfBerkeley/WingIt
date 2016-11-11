3bdcd3148fefb6bbbcddb370d4dcd1ffccd0dea5
Shreya Reddy <reddy.shreya18@gmail.com>
2016-11-11 14:40:43 -0800

    Added Adventure Class

---
+++ app/src/main/java/com/mdb/wingit/wingit/AdventureList.java
@@ -0,0 +1,57 @@
+package com.mdb.wingit.wingit;
+
+import java.util.ArrayList;
+
+/**
+ * Created by reddy on 11/11/2016.
+ */
+
+public class AdventureList {
+    public ArrayList<Adventure> adventureArrayList;
+
+    public AdventureList() {
+        this.adventureArrayList = new ArrayList<>();
+    }
+
+    public ArrayList<Adventure> getArrayList() {
+        return this.adventureArrayList;
+    }
+
+    public void addAdventure(Adventure adventure) {
+        this.adventureArrayList.add(adventure);
+    }
+
+    public static class Adventure {
+        String name;
+        String date;
+        ArrayList<String> activityKeyList;
+        String mapURL;
+
+        public Adventure(String name, String date, ArrayList<String> activityKeyList, String mapURL) {
+            this.name = name;
+            this.date = date;
+            this.activityKeyList = activityKeyList;
+            this.mapURL = mapURL;
+        }
+
+        public static String getName() {
+            return this.name;
+        }
+
+        public static void setName(String newName) {
+            this.name = newName;
+        }
+
+        public static String getDate() {
+            return this.date;
+        }
+
+        public static ArrayList<String> getActivityKeyList() {
+            return this.activityKeyList;
+        }
+
+        public static String getMapURL() {
+            return this.mapURL;
+        }
+    }
+}
