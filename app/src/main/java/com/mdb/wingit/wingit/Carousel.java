85b93a3b1b54344861e584af6ce6018b2a2e7e2f
Katharine Jiang <katharine.jiang9@gmail.com>
2016-11-11 13:04:20 -0800

    added carousel layout manager, has some errors due to missing java classes

--- app/src/main/java/com/mdb/wingit/wingit/Carousel.java
+++ app/src/main/java/com/mdb/wingit/wingit/Carousel.java
@@ -1,26 +1,59 @@
 package com.mdb.wingit.wingit;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.azoft.carousellayoutmanager.CarouselLayoutManager;
+import com.azoft.carousellayoutmanager.CarouselZoomPostLayoutListener;
+import com.azoft.carousellayoutmanager.CenterScrollListener;
+
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
-public class Carousel extends AppCompatActivity {
+public class Carousel extends AppCompatActivity implements View.OnClickListener {
 
     private RecyclerView rv;
     private CarouselAdapter adapter;
     private Button go;
-    private TextView activityName;
-    private ImageView activityPic;
+    private ArrayList<Object> activities;
+    final CarouselLayoutManager layoutManager = new CarouselLayoutManager(CarouselLayoutManager.HORIZONTAL, true);;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carousel);
+
+        adapter = new CarouselAdapter(getApplicationContext(), activities);
+        activities = new ArrayList<>();
+        go = (Button) findViewById(R.id.go);
+        rv = (RecyclerView) findViewById(R.id.rv);
+
+        rv.setLayoutManager(layoutManager);
+        rv.setHasFixedSize(true);
+        rv.setAdapter(adapter);
+        rv.addOnScrollListener(new CenterScrollListener());
+
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.go:
+                Intent intent = new Intent(Carousel.this, Details.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+
 }
--- app/src/main/java/com/mdb/wingit/wingit/CarouselAdapter.java
+++ app/src/main/java/com/mdb/wingit/wingit/CarouselAdapter.java
@@ -1,8 +1,62 @@
 package com.mdb.wingit.wingit;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by KJ on 11/11/16.
  */
 
-public class CarouselAdapter extends  {
+public class CarouselAdapter extends RecyclerView.Adapter<CarouselAdapter.CustomViewHolder> {
+
+    private Context context;
+    private ArrayList<Object> activities = new ArrayList();
+
+    public CarouselAdapter(Context context, ArrayList<Object> activities) {
+        this.context = context;
+        this.activities = activities;
+    }
+
+    @Override
+    public int getItemCount() {
+        return activities.size();
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_view_carousel, parent, false);
+        return new CustomViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final CustomViewHolder holder, int position) {
+        ActivityList.Activity activity = activities.get(position);
+        holder.activityTitle.setText(activity.title);
+        holder.activityPic.setImageURI(activity.imageURI);
+
+    }
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+        TextView activityTitle;
+        ImageView activityPic;
+
+        public CustomViewHolder (View view) {
+            super(view);
+            this.activityTitle = (TextView) view.findViewById(R.id.activityTitle);
+            this.activityPic = (ImageView) view.findViewById(R.id.activityPic);
+
+        }
+    }
+
 }
