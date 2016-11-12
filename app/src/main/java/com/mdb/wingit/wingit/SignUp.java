360faa185ad09c1b546eac7fb744a853c4095cee
Sharie Wang <Sharie Wang>
2016-11-12 10:23:07 -0800

    made sign up screen, added auth

--- app/src/main/java/com/mdb/wingit/wingit/SignUp.java
+++ app/src/main/java/com/mdb/wingit/wingit/SignUp.java
@@ -1,10 +1,31 @@
 package com.mdb.wingit.wingit;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,24 +37,32 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Login extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+public class SignUp extends AppCompatActivity {
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView, name;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    private EditText email, password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
-        //UI elements
-        email = (EditText) findViewById(R.id.email);
-        password = (EditText) findViewById(R.id.password);
-        Button login = (Button) findViewById(R.id.sign_in_button);
-        TextView register = (TextView) findViewById(R.id.textView);
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        name = (EditText) findViewById(R.id.name);
+        Button signUpButton = (Button) findViewById(R.id.email_sign_in_button);
 
-        //FireBase
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -51,20 +80,14 @@ public class Login extends AppCompatActivity {
             }
         };
 
-        register.setOnClickListener(new View.OnClickListener() {
+        signUpButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent openRegister = new Intent(getApplicationContext(), SignUp.class);
-                startActivity(openRegister);
+                createUser(mEmailView.getText().toString(), mPasswordView.getText().toString());
             }
         });
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signIn(email.getText().toString(), password.getText().toString());
-            }
-        });
+
     }
 
     @Override
@@ -81,8 +104,8 @@ public class Login extends AppCompatActivity {
         }
     }
 
-    public void signIn(String email, String password){
-        mAuth.signInWithEmailAndPassword(email, password)
+    public void createUser(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -90,10 +113,14 @@ public class Login extends AppCompatActivity {
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
-                            Toast.makeText(Login.this, "Login failed.",
+                            Toast.makeText(SignUp.this, "Failed to create an account.",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+
     }
+
+
 }
+
