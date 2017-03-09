222a0fa34de22c8a0f92483ce7dfc2d767e5f35e
Sharie Wang <Sharie Wang>
2017-03-09 02:26:34 -0800

    used some random github library for the timeline view, made some progress on the xml

--- app/src/main/java/com/mdb/wingit/wingit/AdventureTimelineAdapter.java
+++ app/src/main/java/com/mdb/wingit/wingit/AdventureTimelineAdapter.java
@@ -1,8 +1,48 @@
 package com.mdb.wingit.wingit;
 
-/**
- * Created by KJ on 2/22/17.
- */
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class AdventureTimelineAdapter {
+import com.github.vipulasri.timelineview.TimelineView;
+
+class AdventureTimelineAdapter extends RecyclerView.Adapter<AdventureTimelineAdapter.TimeLineViewHolder> {
+
+    @Override
+    public int getItemViewType(int position) {
+        return TimelineView.getTimeLineViewType(position, getItemCount());
+    }
+
+    @Override
+    public TimeLineViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = View.inflate(parent.getContext(), R.layout.timeline_card, null);
+        return new TimeLineViewHolder(view, viewType);
+    }
+
+    @Override
+    public void onBindViewHolder(TimeLineViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class TimeLineViewHolder extends RecyclerView.ViewHolder {
+        TimelineView mTimeLineView;
+        ImageView pinImage;
+        TextView pinName;
+
+        TimeLineViewHolder(View view, int viewType) {
+            super(view);
+            mTimeLineView = (TimelineView) view.findViewById(R.id.time_marker);
+            mTimeLineView.initLine(viewType);
+
+            pinImage = (ImageView) view.findViewById(R.id.pinImage);
+            pinName = (TextView) view.findViewById(R.id.pinName);
+        }
+    }
 }
