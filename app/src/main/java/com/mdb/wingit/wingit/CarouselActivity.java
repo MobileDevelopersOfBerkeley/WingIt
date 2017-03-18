fffe411b5983e47ce5e005788899ca05b96dfd70
Kedar Thakkar <kedarthakkar@airbears2-10-142-33-110.airbears2.1918.berkeley.edu>
2017-03-18 13:07:58 -0700

    Deleted imports

--- app/src/main/java/com/mdb/wingit/wingit/CarouselActivity.java
+++ app/src/main/java/com/mdb/wingit/wingit/CarouselActivity.java
@@ -1,39 +1,7 @@
 package com.mdb.wingit.wingit;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.azoft.carousellayoutmanager.CarouselLayoutManager;
-import com.azoft.carousellayoutmanager.CarouselZoomPostLayoutListener;
-import com.azoft.carousellayoutmanager.CenterScrollListener;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
-import com.google.android.gms.location.places.Places;
-import com.google.android.gms.maps.model.LatLng;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-
-import static com.mdb.wingit.wingit.MainActivity.currentLocations;
-import static com.mdb.wingit.wingit.MainActivity.indexPlace;
 
 public class CarouselActivity extends AppCompatActivity {
 
