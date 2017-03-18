7a5add21e7d13f83f1055913880fbc0201c2ddce
Shreya Reddy <reddy.shreya18@gmail.com>
2017-03-18 03:20:45 -0700

    Fixed compilation errors in Carousel

--- app/src/main/java/com/mdb/wingit/wingit/Pin.java
+++ app/src/main/java/com/mdb/wingit/wingit/Pin.java
@@ -5,5 +5,29 @@ package com.mdb.wingit.wingit;
  */
 
 public class Pin {
+    String name;
+    String photoRef;
+    String placeID;
+    String latitude;
+    String longitude;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhotoRef(String pRef) {
+        this.photoRef = pRef;
+    }
+
+    public void setPlaceID(String pID) {
+        this.placeID = pID;
+    }
+
+    public void setLat(String lat) {
+        this.latitude = lat;
+    }
+
+    public void setLon(String lon) {
+        this.longitude = lon;
+    }
 }
