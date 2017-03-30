839608a99fd0fa838b06fb851afc39f0ebf16aa5
Shreya Reddy <reddy.shreya18@gmail.com>
2017-03-30 15:30:20 -0700

    Added AsyncTask for searchRequest in Carousel

--- app/src/main/java/com/mdb/wingit/wingit/CarouselActivity.java
+++ app/src/main/java/com/mdb/wingit/wingit/CarouselActivity.java
@@ -1,10 +1,12 @@
 package com.mdb.wingit.wingit;
 
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.azoft.carousellayoutmanager.CarouselLayoutManager;
 import com.azoft.carousellayoutmanager.CarouselZoomPostLayoutListener;
@@ -13,6 +15,14 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class CarouselActivity extends AppCompatActivity {
@@ -61,7 +71,7 @@ public class CarouselActivity extends AppCompatActivity {
 
         //Compose and send searchRequest based on category user selected
         String searchRequest = createRequestURL(isFoodCategory);
-        sendRequestTask(searchRequest);
+        new RequestTask().execute(searchRequest);
     }
 
     /** Create request URL based on search radius and type associated with selected category */
@@ -88,7 +98,74 @@ public class CarouselActivity extends AppCompatActivity {
         return initMapsURL + locationURL + radiusURL + typesURL + endMapsURL;
     }
 
-    private void sendRequestTask(String request) {
+    //TODO: Clean up this code
+    private class RequestTask extends AsyncTask<String, Void, ArrayList<Pin>> {
+        protected ArrayList<Pin> doInBackground(String... params) {
+            final String request = params[0];
+            HttpURLConnection conn = null;
+            StringBuilder jsonResults = new StringBuilder();
+            ArrayList<Pin> result = new ArrayList<>();
+            try {
+                URL url = new URL(request);
+                conn = (HttpURLConnection) url.openConnection();
+                InputStreamReader in = new InputStreamReader(conn.getInputStream());
 
+                int read;
+                char[] buff = new char[1024];
+                while ((read = in.read(buff)) != -1) {
+                    jsonResults.append(buff, 0, read);
+                    //Log.i("jsonResults length", jsonResults.length()+"");
+                }
+                in.close();
+            } catch (IOException e) {
+                //Log.e("Error", "Error connecting to Places API", e);
+                return result;
+            } finally {
+                if (conn != null) conn.disconnect();
+            }
+            try {
+                // Create a JSON object hierarchy from the results
+                //Log.wtf("length of jsonresults", jsonResults.toString());
+                JSONObject jsonObj = new JSONObject(jsonResults.toString());
+                JSONArray predsJsonArray = jsonObj.getJSONArray("results");
+                //Log.i("size of results", predsJsonArray.length()+"");
+                // Extract the Place descriptions from the results
+                result = new ArrayList<Pin>(predsJsonArray.length());
+                for (int i = 0; i < predsJsonArray.length(); i++) {
+                    //TODO: Need to construct Pin object properly
+                    Pin activity = new Pin();
+                    JSONObject geometry = predsJsonArray.getJSONObject(i).getJSONObject("geometry");
+                    JSONObject location = geometry.getJSONObject("location");
+
+                    if(predsJsonArray.getJSONObject(i).has("photos")) {
+                        JSONArray photos = predsJsonArray.getJSONObject(i).getJSONArray("photos");
+                        if(photos.getJSONObject(0).length()!=0){
+                            JSONObject photoObject = photos.getJSONObject(0);
+                            activity.setPhotoRef(photoObject.getString("photo_reference"));
+                        }
+
+                    }
+                    activity.setName(predsJsonArray.getJSONObject(i).getString("name"));
+                    activity.setPlaceID(predsJsonArray.getJSONObject(i).getString("place_id"));
+                    activity.setLat(location.getString("lat"));
+                    activity.setLon(location.getString("lng"));
+                    result.add(activity);
+                }
+            } catch (JSONException e) {
+                //Log.e("Error", "Error processing JSON results", e);
+            }
+            //Log.wtf("wtf", result.size() + "");
+            return result;
+        }
+
+        protected void onPostExecute(ArrayList<Pin> activityResult) {
+            //TODO: Need to do something with the results returned by searchRequest
+            //result = activityResult;
+            //if(result.size() < 3) {
+                Toast.makeText(CarouselActivity.this, "Could not find any activities at this time", Toast.LENGTH_SHORT).show();
+                finish();
+            //}
+            //randomThrees(result);
+        }
     }
 }
