4d7c035c2ac055e0e25bf7f8f2aee2c8fa7a2d50
Shreya Reddy <reddy.shreya18@gmail.com>
2017-04-01 03:14:02 -0700

    Set up recycler view in timeline activity

--- app/src/main/java/com/mdb/wingit/wingit/activities/AdventureTimelineActivity.java
+++ app/src/main/java/com/mdb/wingit/wingit/activities/AdventureTimelineActivity.java
@@ -3,11 +3,16 @@ package com.mdb.wingit.wingit.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.mdb.wingit.wingit.R;
+import com.mdb.wingit.wingit.modelClasses.Pin;
+
+import java.util.ArrayList;
 
 /**
  * Displays timeline with all the pins of the user's current adventure
@@ -15,10 +20,17 @@ import com.mdb.wingit.wingit.R;
 
 public class AdventureTimelineActivity extends AppCompatActivity {
 
+    private AdventureTimelineAdapter adapter;
+    private ArrayList<Pin> pinList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adventure_timeline);
+
+        RecyclerView rv = (RecyclerView) findViewById(R.id.timeline_recycler_view);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new AdventureTimelineAdapter(this, pinList);
 
         ImageView close = (ImageView) findViewById(R.id.closeButton);
         Button endTrip = (Button) findViewById(R.id.endtrip);
