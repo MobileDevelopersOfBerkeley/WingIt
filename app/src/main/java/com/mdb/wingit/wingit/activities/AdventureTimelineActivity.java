f47d3fdabed4da5653b4b791ff4883f05a2fc3d1
Shreya Reddy <reddy.shreya18@gmail.com>
2017-04-01 14:35:31 -0700

    Finished adventure timeline, has to be tested

--- app/src/main/java/com/mdb/wingit/wingit/activities/AdventureTimelineActivity.java
+++ app/src/main/java/com/mdb/wingit/wingit/activities/AdventureTimelineActivity.java
@@ -1,16 +1,15 @@
 package com.mdb.wingit.wingit.activities;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,25 +36,26 @@ public class AdventureTimelineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adventure_timeline);
 
+        //Set up Timeline Recycler View
         RecyclerView rv = (RecyclerView) findViewById(R.id.timeline_recycler_view);
         rv.setLayoutManager(new LinearLayoutManager(this));
         adapter = new AdventureTimelineAdapter(this, pinList);
 
+        //Read data from Firebase
         //TODO: Get current adventure key from intent extra
         String adventureKey = "";
         dbRef = FirebaseDatabase.getInstance().getReference();
         getFirebaseData(adventureKey);
 
+        //UI Elements
         ImageView close = (ImageView) findViewById(R.id.closeButton);
         Button endTrip = (Button) findViewById(R.id.endtrip);
-
         close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), PinMapActivity.class));
             }
         });
-
         endTrip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,6 +64,7 @@ public class AdventureTimelineActivity extends AppCompatActivity {
         });
     }
 
+    /** Retrieve list of pins from Firebase for specified adventure */
     private void getFirebaseData(String adventureKey) {
         DatabaseReference adventureRef = dbRef.child("Adventures").child(adventureKey);
         adventureRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -76,11 +77,12 @@ public class AdventureTimelineActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.e("Database Error", databaseError.toString());
             }
         });
     }
 
+    /** Update recycler view of pins according to list stored in Firebase */
     private void getPinList(final ArrayList<String> pinKeys) {
         DatabaseReference pinRef = dbRef.child("Pins");
         pinRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -91,15 +93,15 @@ public class AdventureTimelineActivity extends AppCompatActivity {
                         Pin pin = ds.getValue(Pin.class);
                         //TODO: Order pins by startTime
                         pinList.add(pin);
-                        adapter.notifyDataSetChanged();
                     }
                 }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.e("Database Error", databaseError.toString());
             }
         });
+        adapter.notifyDataSetChanged();
     }
 }
