bd37c58e73903dd5794678125519526666129a11
Shreya Reddy <reddy.shreya18@gmail.com>
2017-04-01 14:15:54 -0700

    Read from Firebase for timeline

--- app/src/main/java/com/mdb/wingit/wingit/activities/AdventureTimelineActivity.java
+++ app/src/main/java/com/mdb/wingit/wingit/activities/AdventureTimelineActivity.java
@@ -1,6 +1,7 @@
 package com.mdb.wingit.wingit.activities;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,9 +11,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mdb.wingit.wingit.R;
+import com.mdb.wingit.wingit.modelClasses.Adventure;
 import com.mdb.wingit.wingit.modelClasses.Pin;
 
 import java.util.ArrayList;
@@ -25,6 +30,7 @@ public class AdventureTimelineActivity extends AppCompatActivity {
 
     private AdventureTimelineAdapter adapter;
     private ArrayList<Pin> pinList = new ArrayList<>();
+    private DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +41,10 @@ public class AdventureTimelineActivity extends AppCompatActivity {
         rv.setLayoutManager(new LinearLayoutManager(this));
         adapter = new AdventureTimelineAdapter(this, pinList);
 
-        getFirebaseData();
+        //TODO: Get current adventure key from intent extra
+        String adventureKey = "";
+        dbRef = FirebaseDatabase.getInstance().getReference();
+        getFirebaseData(adventureKey);
 
         ImageView close = (ImageView) findViewById(R.id.closeButton);
         Button endTrip = (Button) findViewById(R.id.endtrip);
@@ -55,10 +64,42 @@ public class AdventureTimelineActivity extends AppCompatActivity {
         });
     }
 
-    private void getFirebaseData() {
-        //TODO: Get current adventure key from intent extra
-        String adventureKey = "";
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+    private void getFirebaseData(String adventureKey) {
         DatabaseReference adventureRef = dbRef.child("Adventures").child(adventureKey);
+        adventureRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Adventure currAdventure = dataSnapshot.getValue(Adventure.class);
+                ArrayList<String> pinKeys = currAdventure.getPinKeysList();
+                getPinList(pinKeys);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getPinList(final ArrayList<String> pinKeys) {
+        DatabaseReference pinRef = dbRef.child("Pins");
+        pinRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    if (pinKeys.contains(ds.getKey())) {
+                        Pin pin = ds.getValue(Pin.class);
+                        //TODO: Order pins by startTime
+                        pinList.add(pin);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
